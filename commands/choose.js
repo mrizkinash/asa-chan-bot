a411cf8ed785ee98a0085206694472fa48cbe91d
mrizkinash <mrizkinash@gmail.com>
2022-09-18 16:43:09 +0700

    Added simple choose command

---
+++ commands/choose.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'choose',
+    description: 'Randomly chooses one argument from all arguments',
+    execute(client, message, args) {
+        if (args.length < 2) {
+            message.channel.send('Usage: ```!choose arg1 arg2 [arg3 argN]```');
+            return;
+        }
+
+        const border = 1.0 / args.length;
+        const choice = Math.floor(Math.random() / border);
+
+        message.channel.send(args[(choice < args.length) ? choice : args.length - 1]);
+    },
+};
