c1a5ee90d33b6922d5f0a1eda7af54283d32dad1
mrizkinash <mrizkinash@gmail.com>
2022-09-25 22:48:31 +0700

    Seperated utilities and made individual kanjis show up on reaction

--- commands/kanji.js
+++ commands/kanji.js
@@ -1,128 +1,90 @@
 const JishoAPI = require('unofficial-jisho-api');
-const { EmbedBuilder } = require('discord.js');
+const kanjiUtils = require('../utils/kanjiUtils.js');
 // TODO: Add reaction to give examples of words starting, ending, or containing the kanji
-// TO DO: Make individual kanji show up with reactions instead of showing all of them
-function createKanjiEmbed(data) {
-    const embed = new EmbedBuilder()
-        .setColor(0x00FF00)
-        .setTitle(data.query)
-        .setURL(data.uri)
-        .addFields(
-            { name: 'JLPT Level', value: (data.jlptLevel || 'Unknown'), inline: true },
-            { name: 'Stroke Count', value: String(data.strokeCount), inline: true },
-            { name: 'Meaning', value: data.meaning },
-            { name: 'Kunyomi', value: (data.kunyomi.join(', ') || '\u200b'), inline: true },
-            { name: 'Onyomi', value: (data.onyomi.join(', ') || '\u200b'), inline: true },
-            { name: '\u200b', value: '\u200b', inline: true },
-            { name: 'Radical', value: data.radical.meaning.concat(' ', data.radical.symbol), inline: true },
-            { name: 'Parts', value: data.parts.join(', '), inline: true },
-            { name: '\u200b', value: '\u200b', inline: true },
-        )
-        .setImage(data.strokeOrderGifUri);
-
-    return embed;
-}
-
-function createPhraseEmbed(data) {
-    const reading = data.japanese
-        .map(japanese => japanese.word.concat(` (${japanese.reading})`))
-        .join(', ');
-
-    let meaning = '';
-    let i = 1;
-
-    for (const item of data.senses) {
-        if (meaning.length != 0) meaning = meaning + '\n';
-
-        const defs = item.english_definitions.join(', ');
-        meaning = meaning.concat(i.toString(), `. ${defs}`);
-        i++;
-    }
-
-    const embed = new EmbedBuilder()
-        .setColor(0x00FF00)
-        .setTitle(data.slug)
-        .setURL(data.uri)
-        .addFields(
-            { name: reading, value: meaning },
-        );
-
-    return embed;
-}
-
-function isKanji(char) {
-    return (char >= '\u4e00' && char <= '\u9faf') || (char >= '\u3400' && char <= '\u4dbf');
-}
-
-function isHiragana(char) {
-    return (char >= '\u3041' && char <= '\u3096');
-}
-
-function isKatakana(char) {
-    return (char >= '\u30A0' && char <= '30FF');
-}
-
-function isJapanese(char) {
-    return (isHiragana(char) || isKatakana(char) || isKanji(char));
-}
 
 module.exports = {
     name: 'kanji',
     description: 'Search for a kanji/phrase in Jisho',
     async execute(client, message, args) {
-        if (args.length != 1 || args[0]?.split('').some(char => !isJapanese(char))) {
+        if (args.length != 1 || Array.from(args[0]).some(char => !kanjiUtils.isJapanese(char))) {
             message.channel.send('Usage: ```!kanji [kanji/japanese phrase]```');
             return;
         }
 
         const jisho = new JishoAPI();
 
-        try {
+        if (args[0].length > 1) {
+            const kanjis = Array.from(args[0]).filter(kanjiUtils.isKanji);
+            const tasks =
+                [jisho.searchForPhrase(args[0]), jisho.scrapeForPhrase(args[0])]
+                    .concat(kanjis.map(kanji => jisho.searchForKanji(kanji)));
 
-            if (args[0].length > 1) {
-                const kanjis = args[0].split('').filter(isKanji);
-                const tasks =
-                    [jisho.searchForPhrase(args[0]), jisho.scrapeForPhrase(args[0])]
-                        .concat(kanjis.map(kanji => jisho.searchForKanji(kanji)));
+            const [phraseResult, phraseURI, ...kanjisResult] =
+                await Promise.allSettled(tasks);
 
-                const [phraseResult, phraseURI, ...kanjisResult] =
-                    await Promise.allSettled(tasks);
+            if (phraseResult.value?.meta.status == 200) {
+                phraseResult.value.data[0].uri = phraseURI.value?.uri;
+                const phraseEmbed = kanjiUtils.createPhraseEmbed(phraseResult.value.data[0]);
 
-                if (phraseResult.value.meta.status == 200) {
-                    phraseResult.value.data[0].uri = phraseURI?.value.uri;
-                    const phraseEmbed = createPhraseEmbed(phraseResult.value.data[0]);
+                let i = 0;
+                const kanjisEmbed = [];
+                while (i < kanjis.length) {
+                    if (kanjisResult[i].value?.found) {
+                        kanjisResult[i].value.query = kanjis[i];
+                        kanjisEmbed.push(kanjiUtils.createKanjiEmbed(kanjisResult[i].value));
+                    }
+                    i++;
+                }
 
-                    let i = 0;
-                    const kanjisEmbed = [];
-                    while (i < kanjis.length) {
-                        if (kanjisResult[i].value.found) {
-                            kanjisResult[i].value.query = kanjis[i];
-                            kanjisEmbed.push(createKanjiEmbed(kanjisResult[i].value));
-                        }
-                        i++;
+                const finalEmbeds = kanjiUtils.getPaginatedEmbed([phraseEmbed, ...kanjisEmbed]);
+
+                message.channel.send({ embeds: [finalEmbeds[0]] }).then(msg => {
+                    const prev = '◀️';
+                    const next = '▶️';
+                    const reactions = [prev, next];
+
+                    for (const reaction of reactions) {
+                        msg.react(reaction);
                     }
 
-                    message.channel.send({ embeds: [phraseEmbed].concat(kanjisEmbed) });
-                } else {
-                    message.channel.send('Phrase not found');
-                }
+                    const filter = (reaction, user) => {
+                        return user.id === message.author.id
+                            && reactions.includes(reaction.emoji.name);
+                    };
 
+                    const switchPage = (reaction) => {
+                        if (reaction.emoji.name === prev) {
+                            i--;
+                            if (i < 0) i = finalEmbeds.length - 1;
+                            msg.edit({ embeds: [finalEmbeds[i]] });
+                        } else if (reaction.emoji.name === next) {
+                            i++;
+                            if (i >= finalEmbeds.length) i = 0;
+                            msg.edit({ embeds: [finalEmbeds[i]] });
+                        }
+                    };
+
+                    const collector = msg.createReactionCollector({ filter, dispose: true });
+
+                    i = 0;
+                    collector.on('collect', switchPage);
+                    collector.on('remove', switchPage);
+                });
             } else {
-                const result = await jisho.searchForKanji(args[0]);
-
-                if (result.found) {
-                    result.query = args[0];
-                    const embed = createKanjiEmbed(result);
-
-                    message.channel.send({ embeds: [embed] });
-                } else {
-                    message.channel.send('Kanji not found');
-                }
+                message.channel.send('Phrase not found');
             }
 
-        } catch (error) {
-            console.log(error);
-            message.channel.send('An error occured while finding kanji');
+        } else {
+            const result = await jisho.searchForKanji(args[0]);
+
+            if (result?.found) {
+                result.query = args[0];
+                const embed = kanjiUtils.createKanjiEmbed(result);
+
+                message.channel.send({ embeds: [embed] });
+            } else {
+                message.channel.send('Kanji not found');
+            }
         }
     },
 };
---
+++ utils/kanjiUtils.js
@@ -0,0 +1,85 @@
+const { EmbedBuilder } = require('discord.js');
+
+function isKanji(char) {
+    return (char >= '\u4e00' && char <= '\u9faf') || (char >= '\u3400' && char <= '\u4dbf');
+}
+
+function isHiragana(char) {
+    return (char >= '\u3041' && char <= '\u3096');
+}
+
+function isKatakana(char) {
+    return (char >= '\u30A0' && char <= '30FF');
+}
+
+function isJapanese(char) {
+    return (isHiragana(char) || isKatakana(char) || isKanji(char));
+}
+
+function createKanjiEmbed(data) {
+    const embed = new EmbedBuilder()
+        .setColor(0x00FF00)
+        .setTitle(data.query)
+        .setURL(data.uri)
+        .addFields(
+            { name: 'JLPT Level', value: (data.jlptLevel || 'Unknown'), inline: true },
+            { name: 'Stroke Count', value: String(data.strokeCount), inline: true },
+            { name: 'Meaning', value: data.meaning },
+            { name: 'Kunyomi', value: (data.kunyomi.join(', ') || '\u200b'), inline: true },
+            { name: 'Onyomi', value: (data.onyomi.join(', ') || '\u200b'), inline: true },
+            { name: '\u200b', value: '\u200b', inline: true },
+            { name: 'Radical', value: data.radical.meaning.concat(' ', data.radical.symbol), inline: true },
+            { name: 'Parts', value: data.parts.join(', '), inline: true },
+            { name: '\u200b', value: '\u200b', inline: true },
+        )
+        .setImage(data.strokeOrderGifUri);
+
+    return embed;
+}
+
+function createPhraseEmbed(data) {
+    const reading = data.japanese
+        .map(japanese => japanese.word.concat(` (${japanese.reading})`))
+        .join(', ');
+
+    let meaning = '';
+    let i = 1;
+
+    for (const item of data.senses) {
+        if (meaning.length != 0) meaning = meaning + '\n';
+
+        const defs = item.english_definitions.join(', ');
+        meaning = meaning.concat(i.toString(), `. ${defs}`);
+        i++;
+    }
+
+    const embed = new EmbedBuilder()
+        .setColor(0x00FF00)
+        .setTitle(data.slug)
+        .setURL(data.uri)
+        .addFields(
+            { name: reading, value: meaning },
+        );
+
+    return embed;
+}
+
+function getPaginatedEmbed(embedList) {
+    let i = 1;
+    for (const embed of embedList) {
+        embed.setFooter({ text: `Page ${i} of ${embedList.length}` });
+        i++;
+    }
+
+    return embedList;
+}
+
+module.exports = {
+    isKanji,
+    isHiragana,
+    isKatakana,
+    isJapanese,
+    createKanjiEmbed,
+    createPhraseEmbed,
+    getPaginatedEmbed,
+};
